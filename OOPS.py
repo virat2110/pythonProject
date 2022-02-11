62ab671a99499db19319001a71dece4825a1cbfd
virat anand <viratanand21@gmail.com>
2022-02-11 15:49:48 +0530

    exception added

--- OOPS.py
+++ OOPS.py
@@ -2,11 +2,11 @@ class Cube:     # class CLASS_NAME:
     x=10
     y=20
     z=40
-    color= ""      # intializing attribute
+    color= ""      # initializing attribute
 
     def volume (self) :   # def NAME(self , <other parameter>)    self works as this
         return self.x*self.y*self.z
-    def __init__(self, name="default"):            # it is a constructor in python so while creating instance we have to give parameters.
-        self.color = name           # intializing attribute
+    def __init__(self, color="default"):            # it is a constructor in python so while creating instance we have to give parameters.
+        self.color = color           # initializing attribute
 
 print(Cube.y)
